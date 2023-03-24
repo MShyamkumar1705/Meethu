dbe82cd8dfd126b3da9f1cacbf99ab737a040f1a
samdhruv05@gmail.com <samdhruv05@gmail.com>
2023-03-24 17:50:03 +0530

    commit 1 for flipkart login

---
+++ src/test/java/GitPractice/FlipkartLogin.java
@@ -0,0 +1,21 @@
+package GitPractice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FlipkartLogin {
+
+	@Test
+	public void login() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.flipkart.com/");
+		Reporter.log("login sucessfull flipkaert=================================================================================================");
+		driver.close();
+	}
+}
