4604fee18ac2364420d7fbe52f765c43576320a8
samdhruv05@gmail.com <samdhruv05@gmail.com>
2023-03-24 18:11:23 +0530

    commit 2 new changes

--- src/test/java/GitPractice/FlipkartLogin.java
+++ src/test/java/GitPractice/FlipkartLogin.java
@@ -2,6 +2,7 @@ package GitPractice;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
@@ -15,7 +16,10 @@ public class FlipkartLogin {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.flipkart.com/");
+		
 		Reporter.log("login sucessfull flipkaert=================================================================================================");
+		driver.findElement(By.name("q")).sendKeys("iphone");
+		Reporter.log("Add Tow Statements");
 		driver.close();
 	}
 }
